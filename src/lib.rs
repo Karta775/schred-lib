2f07da4e819fcd241cdebf2f1f16be2c8dbd24ee
Karta775 <Karta775@users.noreply.github.com>
2023-07-19 12:01:02 +0100

    Added basic shred_file functionality
    The shred_file method overwrites the file with one pass of zeros. Additionally this commit adds some logging features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 use std::path::Path;
 use std::{process, io}; 
 
+const BLOCK_SIZE: usize = 16384;
+
 #[derive(Default)]
 pub struct ShredOptions {
-    verbose: bool,
-    deallocate: bool,
-    recursive: bool,
+    pub verbose: bool,
+    pub deallocate: bool,
+    pub recursive: bool,
 }
 
 #[derive(Debug)]
@@ -23,12 +27,39 @@ impl Shredder {
         return Shredder { options }
     }
 
+    fn write(&self, message: &str) {
+        println!("schred: {}", message);
+    }
+
+    fn log(&self, message: &str) {
+        if self.options.verbose {
+            self.write(message);
+        }
+    }
+
+    fn shred_file(&self, path: &Path) {
+        self.log(&format!("Starting shred of file: {}", path.to_str().unwrap()));
+        let mut buffer = OpenOptions::new().write(true).open(path).unwrap();
+        let mut pos: usize = 0;
+        let zeros: [u8;BLOCK_SIZE] = [0;BLOCK_SIZE];
+        let original_len: usize = buffer.metadata().unwrap().len() as usize;
+        while pos < original_len {
+            self.log(&format!("pos: {}", pos));
+            let bytes_written = buffer.write(&zeros[0..(original_len - pos).min(BLOCK_SIZE)]).unwrap();
+            pos += bytes_written;
+        }
+        buffer.flush().unwrap();
+    }
+
     pub fn shred(&self, path: &Path) -> Result<(), ShredError> {
         if path.is_dir() && !self.options.recursive {
             return Err(ShredError::DirectoryWithoutRecursive);
         }
         if !path.exists() {
             return Err(ShredError::PathDoesntExist);
+        }
+        if path.is_file() {
+            self.shred_file(path);
         }
 
         Ok(())
